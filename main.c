1055f13a5bfe4165f17461897d82fa208b7d8996
Allmeerage <arkan.k06@gmail.com>
2025-05-14 19:33:00 +0700

    Changed main to add a multiplication function

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int addition (int a, int b){
- return a + b;
+	return a + b;
 }
 
+int multiplication (int a, int b) {
+	return a * b;
+}
 int main(){
- return 0;
+	return 0;
 }
